338184aa0affb95a6d3847c251729e8164597988
TempUser <agzamkin@mail.ru>
2025-08-03 18:26:06 +0300

    Update Log.java

--- src/agzam4/Log.java
+++ src/agzam4/Log.java
@@ -3,6 +3,8 @@ package agzam4;
 import java.util.Stack;
 
 import arc.struct.ObjectMap;
+import arc.struct.OrderedMap;
+import arc.util.ColorCodes;
 
 public class Log {
 
@@ -13,6 +15,68 @@ public class Log {
 			colors.put(c.name, c);
 		}
 		reset();
+		
+	    ColorCodes.flush = "\033[H\033[2J";
+	    ColorCodes.reset = "\u001B[0m";
+	    ColorCodes.bold = "\u001B[1m";
+	    ColorCodes.italic = "\u001B[3m";
+	    ColorCodes.underline = "\u001B[4m";
+	    ColorCodes.black = "\u001B[30m";
+	    ColorCodes.red = "\u001B[31m";
+	    ColorCodes.green = "\u001B[32m";
+	    ColorCodes.yellow = "\u001B[33m";
+	    ColorCodes.blue = "\u001B[34m";
+	    ColorCodes.purple = "\u001B[35m";
+	    ColorCodes.cyan = "\u001B[36m";
+	    ColorCodes.lightBlack = "\u001b[90m";
+	    ColorCodes.lightRed = "\u001B[91m";
+	    ColorCodes.lightGreen = "\u001B[92m";
+	    ColorCodes.lightYellow = "\u001B[93m";
+	    ColorCodes.lightBlue = "\u001B[94m";
+	    ColorCodes.lightMagenta = "\u001B[95m";
+	    ColorCodes.lightCyan = "\u001B[96m";
+	    ColorCodes.lightWhite = "\u001b[97m";
+	    ColorCodes.white = "\u001B[37m";
+
+	    ColorCodes.backDefault = "\u001B[49m";
+	    ColorCodes.backRed = "\u001B[41m";
+	    ColorCodes.backGreen = "\u001B[42m";
+	    ColorCodes.backYellow = "\u001B[43m";
+	    ColorCodes.backBlue = "\u001B[44m";
+
+        OrderedMap<String, String> map = OrderedMap.of(
+        "bd", ColorCodes.backDefault,
+        "br", ColorCodes.backRed,
+        "bg", ColorCodes.backGreen,
+        "by", ColorCodes.backYellow,
+        "bb", ColorCodes.backBlue,
+
+        "ff", ColorCodes.flush,
+        "fr", ColorCodes.reset,
+        "fb", ColorCodes.bold,
+        "fi", ColorCodes.italic,
+        "fu", ColorCodes.underline,
+        "k", ColorCodes.black,
+        "lk", ColorCodes.lightBlack,
+        "lw", ColorCodes.lightWhite,
+        "r", ColorCodes.red,
+        "g", ColorCodes.green,
+        "y", ColorCodes.yellow,
+        "b", ColorCodes.blue,
+        "p", ColorCodes.purple,
+        "c", ColorCodes.cyan,
+        "lr", ColorCodes.lightRed,
+        "lg", ColorCodes.lightGreen,
+        "ly", ColorCodes.lightYellow,
+        "lm", ColorCodes.lightMagenta,
+        "lb", ColorCodes.lightBlue,
+        "lc", ColorCodes.lightCyan,
+        "w", ColorCodes.white
+        );
+        String[] values = map.values().toSeq().toArray(String.class);
+        for (int i = 0; i < ColorCodes.values.length; i++) {
+        	ColorCodes.values[i] = values[i];
+		}
 	}
 	
 	public static void init() {
@@ -20,7 +84,7 @@ public class Log {
 	}
 	
 	public static void reset() {
-		System.out.print(Colors.reset.color);
+		arc.util.Log.useColors = true;
 	}
 	
 	public static enum Colors {
@@ -38,9 +102,23 @@ public class Log {
 		reset(0);
 
 		String color, name = name();
-		
+
 		private Colors(int code) {
 			color = code == 0 ? "\033[0m" : "\033[1;" + code + "m";
+			StringBuilder name = new StringBuilder();
+			for (int i = 0; i < this.name.length(); i++) {
+				char c = this.name.charAt(i);
+				if(Character.isUpperCase(c)) {
+					name.append('-');
+					c = Character.toLowerCase(c);
+				}
+				name.append(c);
+			}
+			this.name = name.toString();
+		}
+
+		private Colors(String str) {
+			color = "&" + str;
 			StringBuilder name = new StringBuilder();
 			for (int i = 0; i < this.name.length(); i++) {
 				char c = this.name.charAt(i);
@@ -58,6 +136,35 @@ public class Log {
 			return color;
 		}
 	}
+
+//    "bd", backDefault,
+//    "br", backRed,
+//    "bg", backGreen,
+//    "by", backYellow,
+//    "bb", backBlue,
+//
+//    "ff", flush,
+//    "fr", reset,
+//    "fb", bold,
+//    "fi", italic,
+//    "fu", underline,
+//    "k", black,
+//    "lk", lightBlack,
+//    "lw", lightWhite,
+//    "r", red,
+//    "g", green,
+//    "y", yellow,
+//    "b", blue,
+//    "p", purple,
+//    "c", cyan,
+//    "lr", lightRed,
+//    "lg", lightGreen,
+//    "ly", lightYellow,
+//    "lm", lightMagenta,
+//    "lb", lightBlue,
+//    "lc", lightCyan,
+//    "w", white
+	
 
 	public static void info(Object... args) {
 //		(args);
