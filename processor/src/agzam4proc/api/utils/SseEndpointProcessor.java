ae03de4ff249cb54f5356bab580308f6b87263f2
Agzam4 <75160721+Agzam4@users.noreply.github.com>
2026-06-25 00:53:31 +0300

    feat: sse endpoint apt

---
+++ processor/src/agzam4proc/api/utils/SseEndpointProcessor.java
@@ -0,0 +1,39 @@
+package agzam4proc.api.utils;
+
+import javax.lang.model.element.TypeElement;
+import javax.lang.model.element.VariableElement;
+
+import com.squareup.javapoet.CodeBlock;
+import com.squareup.javapoet.TypeName;
+import com.sun.net.httpserver.HttpExchange;
+
+import agzam4proc.api.ApiSnippets;
+import agzam4proc.api.ApiAnnotations.Sse;
+import agzam4proc.api.utils.init.ConstProvider;
+import agzam4proc.api.utils.init.ConsumerProvider;
+import agzam4proc.api.utils.init.StringProvider;
+import agzam4proc.api.utils.init.VariableInit;
+
+public class SseEndpointProcessor extends EndpointProcessor {
+
+	public SseEndpointProcessor(String prefixValue, TypeElement type, VariableElement field, Sse sse, TypeName object, MethodInfo method) {
+		super(prefixValue, sse.value(), method);
+		
+		DagNode<VariableInit> exchange = new DagNode<>(new ConsumerProvider(
+				"exchange",
+				CodeBlock.of("$T.registerSseEndpoint", TypeName.get(ApiSnippets.class)),
+				new ConstProvider("server"),
+				new StringProvider(prefixValue + "/" + name)
+		));
+		
+		DagNode<VariableInit> message = DagNode.of(new ConsumerProvider(
+				"message",
+				CodeBlock.of("$T.$L.register", type, field.getSimpleName()),
+				exchange.value
+		), exchange);
+		
+		envVariables.put(object, message);
+		envVariables.put(TypeName.get(HttpExchange.class), exchange);
+	}
+
+}
