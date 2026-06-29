fb30cb2846205f561456096558f83b89d5a1f4b8
Agzam4 <75160721+Agzam4@users.noreply.github.com>
2026-06-29 15:53:46 +0300

    feat: more logs endpoints

--- src/agzam4/api/endpoints/ApiLogs.java
+++ src/agzam4/api/endpoints/ApiLogs.java
@@ -9,6 +9,11 @@ import arc.util.Strings;
 
 @Router("/logs")
 public class ApiLogs {
+	
+	/**
+	 * TODO: access check (Dependency that locks it for non-autharisated non-admin users)
+	 * TODO: Search in LogEntity.message support
+	 */
 
 	@Sse
 	public static LogSseSource logsStream = new LogSseSource();
@@ -27,5 +32,16 @@ public class ApiLogs {
 		var entities = Logs.selectByTimerange(from, end, page, pageSize);
 		return Strings.format("[@]", entities.toString(",", e -> Logs.entityJson(e, protect)));
 	}
+	
+	@Post 
+	public static LogEntity[] list(@BodyParm int afterId, @BodyParm int limit) {
+		return new LogEntity[] {}; // TODO
+	}
+	
+	@Post 
+	public static LogEntity[] search(@BodyParm int afterId, @BodyParm int limit, @BodyParm long from, @BodyParm long to, @BodyParm long[] tags, @BodyParm String query) {
+		return new LogEntity[] {}; // TODO: possibly more parameters (for json content possible)
+	}
+	
 
 }
