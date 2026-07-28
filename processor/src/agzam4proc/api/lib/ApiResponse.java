1c4fe3b91306e7bb7a220a36ee5c5f1a0e9acd04
Agzam4 <75160721+Agzam4@users.noreply.github.com>
2026-07-28 04:39:33 +0300

    refactor: api response code construct + constants

--- processor/src/agzam4proc/api/lib/ApiResponse.java
+++ processor/src/agzam4proc/api/lib/ApiResponse.java
@@ -3,11 +3,18 @@ package agzam4proc.api.lib;
 @SuppressWarnings("serial")
 public class ApiResponse extends Exception {
 	
+	public static final ApiResponse forbidden = new ApiResponse("Forbidden", 403);
+	
 	public String content;
 	public int code = 200;
 
 	public ApiResponse(String string) {
 		this.content = string;
+	}
+	
+	public ApiResponse(String string, int code) {
+		this.content = string;
+		this.code = code;
 	}
 
 	public ApiResponse wrongParms() {
@@ -24,6 +31,7 @@ public class ApiResponse extends Exception {
 		return this;
 	}
 
+	@Deprecated
 	public ApiResponse forbidden() {
 		code = 403;
 		return this;
