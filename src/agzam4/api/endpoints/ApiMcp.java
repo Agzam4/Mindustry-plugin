73496f601578fa934b6bfa7e4ccb2c8e931d8f9c
Agzam4 <75160721+Agzam4@users.noreply.github.com>
2026-08-17 00:46:08 +0300

    fix: mcp tokens manage

--- src/agzam4/api/endpoints/ApiMcp.java
+++ src/agzam4/api/endpoints/ApiMcp.java
@@ -9,11 +9,13 @@ import agzam4.admins.Admins;
 import agzam4.commands.Permissions;
 import agzam4.mcp.Mcp;
 import agzam4gen.api.dependencies.Auth;
+import agzam4gen.api.dependencies.BodyParm;
 import agzam4gen.api.dependencies.HeaderParm;
 import agzam4gen.api.dependencies.McpRpc;
 import agzam4proc.apt.api.ApiAnnotations.Parm;
 import agzam4proc.apt.api.ApiAnnotations.Post;
 import agzam4proc.apt.api.ApiAnnotations.Router;
+import agzam4proc.apt.api.ApiAnnotations.Type;
 import agzam4proc.apt.api.lib.ApiResponse;
 import arc.struct.ObjectSet;
 import mindustry.net.Administration.PlayerInfo;
@@ -30,25 +32,39 @@ public class ApiMcp {
 	}
 	
 	@Post
-	public static String[] tokens(@Auth PlayerInfo info) throws ApiResponse, IOException {
+	public static McpSession[] tokens(@Auth PlayerInfo info) throws ApiResponse, IOException {
 		if(!Admins.has(info, Permissions.manageMcp)) throw ApiResponse.forbidden;
-		return Mcp.tokens();
+		var list = Mcp.tokens();
+		McpSession[] result = new McpSession[list.length];
+		for (int i = 0; i < result.length; i++) {
+			result[i] = new McpSession();
+			result[i].token = list[i];
+		}
+		return result;
 	}
 	
 	@Post
 	public static String createToken(@Auth PlayerInfo info) throws ApiResponse, IOException {
 		if(!Admins.has(info, Permissions.manageMcp)) throw ApiResponse.forbidden;
         String token = UUID.randomUUID().toString().replace("-", "");
-		Mcp.createToken(token);
+		var ok = Mcp.createToken(token);
+		if(!ok) throw new ApiResponse("Dublicating token");
 		return token;
 	}
 
 	@Post
-	public static boolean deleteToken(@Auth PlayerInfo info, String token) throws ApiResponse, IOException {
+	public static boolean deleteToken(@Auth PlayerInfo info, @BodyParm String token) throws ApiResponse, IOException {
 		if(!Admins.has(info, Permissions.manageMcp)) throw ApiResponse.forbidden;
 		return Mcp.removeToken(token);
 	}
 	
+	
+	@Type
+	public static class McpSession {
+		
+		public String token;
+	
+	}
 	
 	
 	
