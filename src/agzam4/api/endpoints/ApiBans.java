bd9b17604f6cf50c6748917d35fab6877ca54d03
Agzam4 <75160721+Agzam4@users.noreply.github.com>
2026-09-11 20:56:54 +0300

    feat: bans endpoints

---
+++ src/agzam4/api/endpoints/ApiBans.java
@@ -0,0 +1,62 @@
+package agzam4.api.endpoints;
+
+import agzam4.admins.Admins;
+import agzam4.bans.Bans;
+import agzam4gen.api.dependencies.Auth;
+import agzam4gen.api.dependencies.BodyParm;
+import agzam4proc.apt.api.ApiAnnotations.Post;
+import agzam4proc.apt.api.ApiAnnotations.Router;
+import agzam4proc.apt.api.ApiAnnotations.Type;
+import agzam4proc.apt.api.lib.ApiResponse;
+import mindustry.net.Administration.PlayerInfo;
+
+@Router("/bans")
+public class ApiBans {
+
+	@Type
+	public static class IpListInfo {
+		
+		public String name;
+		public long size;
+		
+	}
+	
+    @Post
+    public static IpListInfo[] groups(@Auth PlayerInfo player) throws ApiResponse {
+    	if(!Admins.has(player, "banslist")) throw ApiResponse.forbidden;
+    	return new IpListInfo[] {
+    			new IpListInfo() {{
+    					name = "bots";
+    					size = Bans.bots.list.size();
+    			}}
+    	};
+    }
+
+    @Post
+    public static long add(@Auth PlayerInfo player, @BodyParm String group, @BodyParm String cidr) throws ApiResponse {
+    	if(!Admins.has(player, "banslist")) throw ApiResponse.forbidden;
+    	Bans.bots.list.addSubnet(cidr);
+    	return Bans.bots.list.size();
+    }
+
+    @Post
+    public static long remove(@Auth PlayerInfo player, @BodyParm String group, @BodyParm String cidr) throws ApiResponse {
+    	if(!Admins.has(player, "banslist")) throw ApiResponse.forbidden;
+    	Bans.bots.list.removeSubnet(cidr);
+    	return Bans.bots.list.size();
+    }
+
+    @Post
+    public static boolean test(@Auth PlayerInfo player, @BodyParm String group, @BodyParm String ip) throws ApiResponse {
+    	if(!Admins.has(player, "banslist")) throw ApiResponse.forbidden;
+    	return Bans.bots.has(ip);
+    }
+    
+
+    @Post
+    public static String get(@Auth PlayerInfo player, @BodyParm String group, @BodyParm String cidr) throws ApiResponse {
+    	if(!Admins.has(player, "banslist")) throw ApiResponse.forbidden;
+    	return Bans.bots.list.file.readString();
+    }
+	
+}
